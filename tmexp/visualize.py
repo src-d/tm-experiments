5451ffbba36b359833909fa2bd6421bee487571d
Romain Keramitas <r.keramitas@gmail.com>
2019-08-20 16:24:46 +0200

    Refactored visualize
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

--- tmexp/visualize.py
+++ tmexp/visualize.py
@@ -1,13 +1,14 @@
 from argparse import ArgumentParser
 import os
 import pickle
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import matplotlib.pyplot as plt
 import numpy as np
 import tqdm
 
 from .cli import CLIBuilder, register_command
+from .data import RefList
 from .io_constants import (
     BOW_DIR,
     EVOLUTION_FILENAME,
@@ -18,7 +19,13 @@ from .io_constants import (
     TOPICS_DIR,
     VIZ_DIR,
 )
-from .utils import check_file_exists, check_remove, create_directory, create_logger
+from .utils import (
+    check_file_exists,
+    check_remove,
+    create_directory,
+    create_logger,
+    load_refs_dict,
+)
 
 
 def _define_parser(parser: ArgumentParser) -> None:
@@ -29,7 +36,7 @@ def _define_parser(parser: ArgumentParser) -> None:
     parser.add_argument(
         "--max-topics",
         help="Limit to this amount the number of topics displayed on visualizations "
-        "simultaniously (will select most interesting), defaults to %(default)s.",
+        "simultaniously (will select most distinct), defaults to %(default)s.",
         default=10,
         type=int,
     )
@@ -37,29 +44,32 @@ def _define_parser(parser: ArgumentParser) -> None:
 
 def create_heatmap(
     output_path: str,
-    data: np.array,
+    data: np.ndarray,
     title: str,
-    vmin: float,
-    vmax: float,
-    x_label: str,
-    y_label: str,
-    x_ticks: Optional[List[float]] = None,
-    y_ticks: Optional[List[float]] = None,
-    x_labels: Optional[List[str]] = None,
-    y_labels: Optional[List[str]] = None,
+    y_labels: List[str],
+    x_label: Optional[str] = None,
+    x_labels: Optional[Union[List[str], RefList]] = None,
 ) -> None:
     plt.figure(figsize=data.T.shape)
     heatmap = plt.pcolor(
-        data, cmap=plt.cm.Blues, vmin=vmin, vmax=vmax, edgecolor="white", linewidths=1
+        data,
+        cmap=plt.cm.Blues,
+        vmin=0.0,
+        vmax=max(np.max(data), 1.0),
+        edgecolor="white",
+        linewidths=1,
     )
     plt.colorbar(heatmap)
     plt.title(title, fontsize=18)
+    if x_label is None:
+        x_label = "Topics"
+        x_labels = y_labels
     plt.xlabel(x_label, fontsize=16)
-    plt.ylabel(y_label, fontsize=16)
-    if x_ticks is not None and x_labels is not None:
-        plt.xticks(x_ticks, x_labels, rotation=90, fontsize=12)
-    if y_ticks is not None and y_labels is not None:
-        plt.yticks(y_ticks, y_labels, fontsize=12)
+    plt.ylabel("Topics", fontsize=16)
+    x_ticks = [i + 0.5 for i, _ in enumerate(x_labels)]
+    plt.xticks(x_ticks, x_labels, rotation=90, fontsize=12)
+    y_ticks = [i + 0.5 for i, _ in enumerate(y_labels)]
+    plt.yticks(y_ticks, y_labels, fontsize=12)
     plt.tick_params(axis="both", which="both", bottom=False, left=False)
     plt.savefig(output_path, bbox_inches="tight")
 
@@ -69,7 +79,6 @@ def visualize(
     bow_name: str, exp_name: str, force: bool, max_topics: int, log_level: str
 ) -> None:
     """Create visualizations for precomputed metrics."""
-    # TODO: include topic names when inference is added
 
     logger = create_logger(log_level, __name__)
 
@@ -85,77 +94,78 @@ def visualize(
     output_dir = os.path.join(VIZ_DIR, bow_name, exp_name)
     check_remove(output_dir, logger, force, is_dir=True)
     create_directory(output_dir, logger)
-    evolution_output_dir = os.path.join(output_dir, "evolution_per_topic")
-    create_directory(evolution_output_dir, logger)
 
-    logger.info("Loading tagged refs ...")
-    with open(refs_input_path, "r", encoding="utf-8") as fin:
-        refs = [ref.replace("refs/tags/", "") for ref in fin.read().split("\n")]
-    logger.info("Loaded tagged refs, found %d." % len(refs))
+    refs_dict = load_refs_dict(logger, refs_input_path)
 
     logger.info("Loading metrics ...")
     with open(metrics_input_path, "rb") as fin_b:
         metrics = pickle.load(fin_b)
-    num_topics = metrics["distinctness"].shape[0]
+    num_topics = metrics.distinctness.shape[0]
     logger.info("Loaded metrics, found %d topics." % num_topics)
 
     logger.info("Loading topic labels ...")
     with open(labels_input_path, "r", encoding="utf-8") as fin:
         topic_labels = [label for label in fin.read().split("\n")]
     logger.info("Loaded topic labels.")
+    if num_topics > max_topics:
+        logger.info("Selecting most distinct topics ...")
+        topic_distinctness = np.sum(metrics.distinctness, axis=1) / (num_topics - 1)
+        topics = np.argsort(topic_distinctness)[::-1][:max_topics]
+        topic_labels = [topic_labels[ind_label] for ind_label in topics]
+        logger.info("Selected the following topics:\n\t%s", "\n\t".join(topic_labels))
 
-    logger.info(
-        "Creating and saving evolution plots for assignment, weight, scatter and focus "
-        "per topic ..."
-    )
-    ref_ticks = [i + 0.5 for i in range(len(refs))]
-    for ind_topic in tqdm.tqdm(range(num_topics)):
-        plt.figure(figsize=(10, 5))
-        for metric_name, metric in metrics.items():
-            if metric_name == "distinctness":
-                continue
-            plt.plot(ref_ticks, metric[:, ind_topic], "-+", label=metric_name)
-        plt.xlabel("Tagged reference", fontsize=14)
-        plt.xticks(ref_ticks, refs, rotation=45)
-        plt.ylabel("Metric value", fontsize=14)
-        plt.ylim(0, 1)
-        plt.title("Metrics for topic '%s'" % topic_labels[ind_topic], fontsize=18)
-        plt.legend(fontsize=16)
-        plt.savefig(
-            os.path.join(evolution_output_dir, EVOLUTION_FILENAME % (ind_topic + 1)),
-            bbox_inches="tight",
-        )
-    logger.info("Saved evolution plots in '%s'." % evolution_output_dir)
+    logger.info("Creating and saving distinctness heatmap ...")
+    output_path = os.path.join(output_dir, HEATMAP_FILENAME % "distinctness")
+    data = metrics.distinctness[topics][:, topics]
+    create_heatmap(output_path, data, "Topic distinctness", topic_labels)
+    logger.info("Saved distinctness heatmap in '%s'", output_path)
 
-    logger.info("Creating and saving heatmaps  per metric ...")
-    for metric_name, metric in metrics.items():
-        output_path = os.path.join(output_dir, HEATMAP_FILENAME % metric_name)
-        if metric_name == "distinctness":
-            label = "Topic index"
-            title = "Topic distinctness"
-            create_heatmap(output_path, metric, title, 0, np.max(metric), label, label)
-        else:
-            metric = metric.T
-            topics = np.argsort(np.max(metric, axis=1))[::-1][:max_topics]
-            metric = metric[topics]
-            labels = [topic_labels[ind_topic] for ind_topic in topics]
-            title = (
-                "Evolution of top %d topics for %s metrics per topic and version"
-                % (max_topics, metric_name)
-            )
-            create_heatmap(
-                output_path,
-                metric,
-                title,
-                0,
-                1,
-                "Tagged reference",
-                "Topic",
-                ref_ticks,
-                [i + 0.5 for i in range(max_topics)],
-                refs,
-                labels,
-            )
+    num_repos = len(refs_dict)
+    if num_repos == 1:
+        repo, x_labels = refs_dict.popitem()
         logger.info(
-            "Created and saved heatmap for %s metric in '%s'", metric_name, output_path
+            "Found only one repository, creating evolution plots per topic ...."
         )
+        x_label = "Tagged references"
+        for ind_topic, topic_label in tqdm.tqdm(
+            enumerate(topic_labels), total=max_topics
+        ):
+            plt.figure(figsize=(10, 5))
+            x_ticks = [i + 0.5 for i in range(len(x_labels))]
+            for metric_ind, metric_name in enumerate(metrics._fields):
+                if metric_name == "distinctness":
+                    continue
+                metric = metrics[metric_ind][repo]
+                plt.plot(x_ticks, metric[:, topics[ind_topic]], "-+", label=metric_name)
+            plt.xlabel(x_label, fontsize=14)
+            plt.xticks(x_ticks, x_labels, rotation=45)
+            plt.ylabel("Metric value", fontsize=14)
+            plt.ylim(0, 1)
+            plt.title("Metrics for topic '%s'" % topic_label, fontsize=18)
+            plt.legend(fontsize=16)
+            plt.savefig(
+                os.path.join(output_dir, EVOLUTION_FILENAME % (ind_topic + 1)),
+                bbox_inches="tight",
+            )
+        logger.info("Saved evolution plots.")
+    if num_repos > 1:
+        logger.info("Found %d repositories, (assuming one ref per repo).", num_repos)
+        x_labels = sorted(refs_dict)  # type: ignore
+        x_label = "Repositories"
+
+    logger.info("Creating heatmaps for each metric ...")
+    for metric_ind, metric_name in enumerate(metrics._fields):
+        if metric_name == "distinctness":
+            continue
+        metric = metrics[metric_ind]
+        output_path = os.path.join(output_dir, HEATMAP_FILENAME % metric_name)
+        if num_repos == 1:
+            data = metric[repo][:, topics].T
+            title = "Topics %s across versions" % metric_name
+        else:
+            data = np.empty((num_topics, num_repos))
+            for ind_repo, repo in enumerate(x_labels):
+                data[:, ind_repo] = metric[repo][0][topics]
+            title = "Topics %s across repositories" % metric_name
+        create_heatmap(output_path, data, title, topic_labels, x_label, x_labels)
+    logger.info("Saved heatmaps.")
